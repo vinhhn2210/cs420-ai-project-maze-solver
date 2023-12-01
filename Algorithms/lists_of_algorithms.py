5aaec8e3f0c9e551fab2d5e4c0e4ae94dcd61474
vinhhn2210 <nhuvinh2210@mail.com>
2023-12-01 18:22:35 +0700

    fix succersor function + move for level 3

---
+++ Algorithms/lists_of_algorithms.py
@@ -0,0 +1,5 @@
+algo_lists = {'level1': ['dfs', 'bfs', 'ucs', 'astar'],
+              'level2': ['dfs', 'bfs'],
+              'level3': ['dfs', 'bfs'],
+              'level4': ['dfs', 'bfs'],
+              }
